eceace25c842330009d2e99508e7975082a8d0f6
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-05-05 23:48:35 -0300

    CRUD test done

--- app.py
+++ app.py
@@ -3,21 +3,36 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dataTest
 user = db.user
-res = user.find()
+read = user.find()
 
-""" post = {
+# Insert funcional
+post = {
     'name': 'Random Guy',
     'age': 65,
     'email': "v-v@email.com" 
 }
 user_id = user.insert_one(post).inserted_id 
 
-print("Id do usuario ",user_id) """
-for doc in res:
+print("Id do usuario ",user_id)
+
+# Update funcional
+
+myQuery = {"name": "Someone-else"}
+newValue = {"$set": {"name": "Nadiya Ismail"}}
+user.update_one(myQuery, newValue)
+
+# Delete funcional
+try:
+    user.delete_one({"name": "Random Guy"})
+except:
+    print("Erro")
+
+
+# Read funcional
+
+for doc in read:
     print(doc)
 
-# Insert funcional
-# Read funcional
-# Update funcional
-# Delete funcional
 # Tela de opções do usuario (CRUD)
+# Banco na nuvem (CLUSTER)
+# UPAR para repl.it
