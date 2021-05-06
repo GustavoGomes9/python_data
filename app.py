7df655b1363a5106e2ad11ab734663cd44cba46c
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-05-06 02:48:40 -0300

    Tela de opçoes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
+import os
 
 client = MongoClient('localhost', 27017)
 db = client.dataTest
 user = db.user
 read = user.find()
-
+"""
 # Insert funcional
 post = {
     'name': 'Random Guy',
@@ -32,7 +33,36 @@ except:
 
 for doc in read:
     print(doc)
+"""
+# loop de interação
+while True:
+    # Tela inicial de opções do usuario (CRUD)
+    print('=============================================')
+    print('                 PYTHON_DATA                 ')
+    print('=============================================')
+    print('Escolha uma opção abaixo: ')
+    print(' [1]-Cadastrar\n [2]-Listar\n [3]-Sair do sistema' )
+    print('=============================================')
+    option = input('Digite aqui: ')
 
-# Tela de opções do usuario (CRUD)
+    # Rotas para opções
+    if option == '1':
+        os.system('clear')
+        print('Opção 1')
+    elif option == '2':
+        os.system('clear')
+        print('Opção 2')
+    elif option == '3':
+        os.system('clear')
+        print('Opção 3')
+    else:
+        print("Entrada não correspondente, escolha um número acima")
+
+
+# Tela cadastro
+# Tela mostrar
+    # Pesquisar
+    # opção atualizar
+    # opção deletar 
 # Banco na nuvem (CLUSTER)
 # UPAR para repl.it
