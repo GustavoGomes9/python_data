4c18402c0e85dfa62be123849960612541d0e6c8
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-05-05 22:00:51 -0300

    Testing crud

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dataTest
 user = db.user
+res = user.find()
 
-post = {
-    'name': 'Ben Otto',
-    'age': 12,
-    'email': "toot@email.com" 
+""" post = {
+    'name': 'Random Guy',
+    'age': 65,
+    'email': "v-v@email.com" 
 }
-user_id = user.insert_one(post).inserted_id
+user_id = user.insert_one(post).inserted_id 
 
-print("Id do usuario ",user_id)
+print("Id do usuario ",user_id) """
+for doc in res:
+    print(doc)
 
+# Insert funcional
+# Read funcional
+# Update funcional
+# Delete funcional
+# Tela de opções do usuario (CRUD)
