6d31d2da1425589e249b2f5124cf333040f9d039
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-05-06 04:25:38 -0300

    Funcional CREATE

--- app.py
+++ app.py
@@ -5,16 +5,6 @@ client = MongoClient('localhost', 27017)
 db = client.dataTest
 user = db.user
 read = user.find()
-"""
-# Insert funcional
-post = {
-    'name': 'Random Guy',
-    'age': 65,
-    'email': "v-v@email.com" 
-}
-user_id = user.insert_one(post).inserted_id 
-
-print("Id do usuario ",user_id)
 
 # Update funcional
 
@@ -22,6 +12,7 @@ myQuery = {"name": "Someone-else"}
 newValue = {"$set": {"name": "Nadiya Ismail"}}
 user.update_one(myQuery, newValue)
 
+"""
 # Delete funcional
 try:
     user.delete_one({"name": "Random Guy"})
@@ -32,8 +23,7 @@ except:
 # Read funcional
 
 for doc in read:
-    print(doc)
-"""
+    print(doc)"""
 # loop de interação
 while True:
     # Tela inicial de opções do usuario (CRUD)
@@ -48,15 +38,48 @@ while True:
     # Rotas para opções
     if option == '1':
         os.system('clear')
-        print('Opção 1')
+        sure = False
+        while not sure:
+            print('=============================================')
+            print('                 CADASTRO                 ')
+            print('=============================================')
+            name = input('Digite seu nome: ')
+            age = int(input("Digite sua idade: "))
+            email = input('Digite seu email: ')
+            os.system('clear')
+            answer = input(f"Seus dados são: \n\n nome: {name}\n idade: {age}\n email: {email}\n\nOs dados estão corretos? [Y/n]: ").lower()
+            if answer == 'y':
+                insert = {
+                    "name": name,
+                    "age": age,
+                    "email": email
+                }
+                try:
+                    insert_id = user.insert_one(insert).inserted_id
+                except:
+                    print("Ocorreu algum erro")
+                else:
+                    print("Cadastro realizado com sucesso!")
+                    sure = True
+            elif answer == 'n':
+                os.system('clear')
+                print("Tente novamente!")
+            else:
+                os.system('clear')
+                print("Valor inserido não correspondente.")
+                sure = True
+
+
+
     elif option == '2':
         os.system('clear')
         print('Opção 2')
     elif option == '3':
         os.system('clear')
-        print('Opção 3')
+        print('Até mais!!')
+        break
     else:
-        print("Entrada não correspondente, escolha um número acima")
+        print("Entrada não correspondente, Tente novamente.")
 
 
 # Tela cadastro
